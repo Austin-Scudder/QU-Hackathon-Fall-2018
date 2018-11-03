29e74827a67d8ed61c478ca46f7f59c0d4956e0b
Austin <AustinS@austinos-mbp.quinnipiac.edu>
2018-11-03 14:49:50 -0400

    Test add storyboards

---
+++ BetterTest/BetterTest/Event_Handler.swift
@@ -0,0 +1,13 @@
+//
+//  Event_Handler.swift
+//  BetterTest
+//
+//  Created by Austin Scudder on 11/3/18.
+//  Copyright © 2018 Austin Scudder. All rights reserved.
+//
+
+import UIKit
+
+class Event_Handler: NSObject {
+    
+}
