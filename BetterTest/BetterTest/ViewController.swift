bac27397268e8897d8f74ed25b4d86a0c733e97c
Austin <AustinS@austinos-mbp.quinnipiac.edu>
2018-11-03 11:29:33 -0400

    Init
    init

---
+++ BetterTest/BetterTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterTest
+//
+//  Created by Austin Scudder on 11/3/18.
+//  Copyright © 2018 Austin Scudder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
